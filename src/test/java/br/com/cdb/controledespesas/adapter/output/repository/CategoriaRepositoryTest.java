75a30d89e5a1f8bca2499afa9adcae4b0631b56b
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-28 20:08:33 -0300

    teste salvar categoria repository

---
+++ src/test/java/br/com/cdb/controledespesas/adapter/output/repository/CategoriaRepositoryTest.java
@@ -0,0 +1,60 @@
+package br.com.cdb.controledespesas.adapter.output.repository;
+
+import br.com.cdb.controledespesas.adapter.input.mapper.CategoriaMapper;
+import br.com.cdb.controledespesas.adapter.output.entity.CategoriaEntity;
+import br.com.cdb.controledespesas.core.domain.model.Categoria;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class CategoriaRepositoryTest {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @Mock
+    private CategoriaMapper categoriaMapper;
+
+    @InjectMocks
+    private CategoriaRepository categoriaRepository;
+
+    private Categoria categoria;
+    private CategoriaEntity entity;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        categoria = new Categoria(1L, "Categoria1");
+        entity = new CategoriaEntity(1L, "Categoria1");
+    }
+
+    @Test
+    void testSalvarCategoria() {
+        when(categoriaMapper.toEntity(categoria)).thenReturn(entity);
+        when(categoriaMapper.toDomain(any(CategoriaEntity.class))).thenReturn(categoria);
+
+        when(jdbcTemplate.execute(any(ConnectionCallback.class)))
+                .then(invocation -> categoriaMapper.toDomain(entity));
+
+        Categoria result = categoriaRepository.salvarCategoria(categoria);
+
+        assertNotNull(result);
+        assertEquals("Categoria1", result.getNome());
+        verify(categoriaMapper).toEntity(any());
+        verify(categoriaMapper).toDomain(any(CategoriaEntity.class));
+    }
+
+
+}
--- target/generated-sources/annotations/br/com/cdb/controledespesas/adapter/input/mapper/CategoriaMapperImpl.java
+++ target/generated-sources/annotations/br/com/cdb/controledespesas/adapter/input/mapper/CategoriaMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-25T19:21:23-0300",
+    date = "2025-09-27T13:45:28-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
