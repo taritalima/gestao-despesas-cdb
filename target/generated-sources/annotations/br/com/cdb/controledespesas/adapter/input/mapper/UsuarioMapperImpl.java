cb5b70bbe8ef23a3da460241f03b732dc63662af
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-10-01 21:44:25 -0300

    teste 85% coberto

--- target/generated-sources/annotations/br/com/cdb/controledespesas/adapter/input/mapper/UsuarioMapperImpl.java
+++ target/generated-sources/annotations/br/com/cdb/controledespesas/adapter/input/mapper/UsuarioMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-01T18:02:41-0300",
+    date = "2025-10-01T21:34:40-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
